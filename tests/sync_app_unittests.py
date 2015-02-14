7a2d959ee5c319f380fb8686bb3e6a6741c960c4
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-14 16:16:14 -0500

    fix test_file_list_local, add test_file_list_cache

--- tests/sync_app_unittests.py
+++ tests/sync_app_unittests.py
@@ -11,6 +11,7 @@ import unittest
 CURDIR = os.path.abspath(os.curdir)
 os.sys.path.append(CURDIR)
 
+from sync_app.file_cache import FileListCache
 from sync_app.file_list_local import FileInfoLocal, FileListLocal
 
 TEST_FILE = 'tests/test_dir/hello_world.txt'
@@ -21,22 +22,47 @@ class TestGarminApp(unittest.TestCase):
         pass
 
     def tearDown(self):
+        if os.path.exists('.tmp_file_list_cache.pkl.gz'):
+            os.remove('.tmp_file_list_cache.pkl.gz')
         pass
 
     def test_file_info_local(self):
         finfo = FileInfoLocal(fn=TEST_FILE)
         output = ('%s' % finfo).replace(CURDIR, '')
-        print output
+        #print output
         m = hashlib.md5()
         m.update(output)
-        
-        self.assertEqual(m.hexdigest(), '84bd4e40788a20e79ef5ead344ff1cd3')
-        pass
+        self.assertEqual(m.hexdigest(), '41c5f9381ea74a6dbc00047e425054a1')
 
     def test_file_list_local(self):
         flist = FileListLocal()
         flist.fill_cache_file_list_local(directory='tests/test_dir')
-        print flist
+        output = []
+        for fl in flist.filelist:
+            output.append(('%s' % fl).replace(CURDIR, ''))
+        output = sorted(output)
+        #print '\n'.join(output)
+        m = hashlib.md5()
+        for out in sorted(output):
+            m.update(out)
+        self.assertEqual(m.hexdigest(), '2880a2249709a08270c7c3575bc31cb5')
+
+    def test_file_list_cache(self):
+        flist = FileListLocal()
+        flist.fill_cache_file_list_local(directory='tests/test_dir')
+        fcache = FileListCache(pickle_file='.tmp_file_list_cache.pkl.gz')
+        fcache.write_cache_file_list(flist.filelist)
+        del flist
+        flist = fcache.get_cache_file_list()
+        output = []
+        for fl in flist.filelist:
+            output.append(('%s' % fl).replace(CURDIR, ''))
+        output = sorted(output)
+        #print '\n'.join(output)
+        m = hashlib.md5()
+        for out in sorted(output):
+            m.update(out)
+        self.assertEqual(m.hexdigest(), '2880a2249709a08270c7c3575bc31cb5')
 
 if __name__ == '__main__':
     unittest.main()
