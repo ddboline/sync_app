f6720e20a40578b9e1863e45c8e435ee92113ebc
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-14 15:32:06 -0500

    create empty file_stat object by default, define interface in file_list.py

--- sync_app/file_list.py
+++ sync_app/file_list.py
@@ -13,40 +13,44 @@
         dicts to efficiently search within filelist
 '''
 import os
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 
 class FileInfo(object):
     ''' file info class '''
     __slots__ = ['filename', 'urlname', 'md5sum', 'filestat']
 
     stat_attrs = ('st_atime', 'st_blksize', 'st_blocks', 'st_ctime', 'st_dev', 'st_gid', 'st_ino', 'st_mode', 'st_mtime', 'st_nlink', 'st_rdev', 'st_size', 'st_uid')
-
-    def __init__(self, fn='', pn='', hn='localhost', url='', md5='', fs=None):
+    
+    stat_tuple = namedtuple('empty_stat_result', stat_attrs)
+    
+    empty_stat = stat_tuple(**{attr: 0 for attr in stat_attrs})
+    
+    def __init__(self, fn='', url='', md5='', fs=None):
         self.filename = fn
         self.urlname = url
-        self.md5sum = md5
-        self.filestat = fs
-        if urlname[:7] == 'file://' and os.path.exists('%s' % self.filename):
-            _md5 = get_md5_full('%s' % self.filename)
-            self.md5sum = _md5
-            _fstat = os.stat(self.filename)
-            self.filestat = {attr: getattr(_fstat, attr) for attr in self.stat_attrs}
+        if md5:
+            self.md5sum = md5
+        else:
+            self.md5sum = self.get_md5()
+        self.filestat = None
+        if fs:
+            if all(hasattr(fs, attr) for attr in stat_attrs):
+                self.filestat = fs
+        if not self.filestat:
+            self.filestat = self.get_stat()
 
     def __repr__(self):
-        return '<FileInfo(fn=%s, hs=%s, md5=%s, size=%s>' % (self.fullfilename,
-      
+        return '<FileInfo(fn=%s, url=%s, md5=%s, size=%s>' % (self.filename, self.urlname, self.md5sum, self.filestat.st_size)
+    
+    def get_md5(self):
+        return None
+    
+    def get_stat(self):
+        return None
 
 class FileList(object):
     ''' file list class '''
-    def __init__(self, basepath='', subdirectories=None, filelist=None):
-        if basepath:
-            self.basepath = basepath
-        else:
-            self.basepath = '.'
-        if subdirectories:
-            self.subdirs = ['%s/%s' % (self.basepath, subdirectories)]
-        else:
-            self.subdirs = [self.basepath]
+    def __init__(self, filelist=None):
         self.filelist = []
         self.filelist_name_dict = defaultdict(list)
         self.filelist_md5_dict = defaultdict(list)
@@ -59,8 +63,8 @@ class FileList(object):
         for at in ['filename', 'urlname', 'md5sum', 'filestat']:
             if not hasattr(file_info_obj, at):
                 return False
-        fn = os.path.basename(self.file_info_obj)
-        md = self.file_info_obj.md5sum
+        fn = os.path.basename(file_info_obj.filename)
+        md = file_info_obj.md5sum
         self.filelist.append(file_info_obj)
         self.filelist_name_dict[fn].append(file_info_obj)
         self.filelist_md5_dict[md].append(file_info_obj)
