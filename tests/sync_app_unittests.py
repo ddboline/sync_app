be789f9c671e8f78b1c31560135460576b1ac42a
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-14 15:34:34 -0500

    test_file_info_local, test_file_list_local

--- tests/sync_app_unittests.py
+++ tests/sync_app_unittests.py
@@ -3,7 +3,17 @@
 '''
     Unit tests for sync_app
 '''
+import os
+import re
+import hashlib
 import unittest
+
+CURDIR = os.path.abspath(os.curdir)
+os.sys.path.append(CURDIR)
+
+from sync_app.file_list_local import FileInfoLocal, FileListLocal
+
+TEST_FILE = 'tests/test_dir/hello_world.txt'
 
 class TestGarminApp(unittest.TestCase):
 
@@ -13,8 +23,20 @@ class TestGarminApp(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_file_list(self):
+    def test_file_info_local(self):
+        finfo = FileInfoLocal(fn=TEST_FILE)
+        output = ('%s' % finfo).replace(CURDIR, '')
+        print output
+        m = hashlib.md5()
+        m.update(output)
+        
+        self.assertEqual(m.hexdigest(), '84bd4e40788a20e79ef5ead344ff1cd3')
         pass
+
+    def test_file_list_local(self):
+        flist = FileListLocal()
+        flist.fill_cache_file_list_local(directory='tests/test_dir')
+        print flist
 
 if __name__ == '__main__':
     unittest.main()
