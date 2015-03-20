e432999bb96ad569b8376b2bcb2a330761e49a9c
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-20 19:05:08 -0400

    ...

--- sync.py
+++ sync.py
@@ -11,6 +11,7 @@ from sync_app.gdrive_instance import GdriveInstance
 from sync_app.file_cache import FileListCache
 from sync_app.file_list_gdrive import FileListGdrive
 from sync_app.file_list_s3 import FileListS3
+from sync_app.file_sync import FileSync
 
 def build_indicies():
     print 'build gdrive'
@@ -26,16 +27,30 @@ def build_indicies():
         for subdir in ['dilepton2_backup', 'dilepton_tower_backup']:
             local_dirs.append('%s/%s' % (basedir, subdir))
     print 'build local %s' % ' '.join(local_dirs)
-    build_local_index(directories=local_dirs, build_local_index=True)
+    build_local_index(directories=local_dirs)
 
 def compare_local():
-    flists_ = []
+    local_dirs = []
     for basedir in ['/home/ddboline', '/media/sabrent2000', '/media/caviar2000', '/media/western2000']:
-        flists_.append('%s/Documents/mp3' % basedir)
+        local_dirs.append('%s/Documents/mp3' % basedir)
+
+    fcache = FileListCache(pickle_file='%s/.local_file_list_cache.pkl.gz' % os.getenv('HOME'))
+    fcache.get_cache_file_list(file_list_class=FileListLocal)
+    flists_ = []
+    for d in local_dirs:
+        flist = FileListLocal()
+        flist.fill_file_list_local(directory=d)
+        flists_.append(flist)
+        
+    flist = FileListLocal()
+    if not rebuild_index:
+        flist = fcache.get_cache_file_list(file_list_class=FileListLocal)
+    for direc in directories:
+        flist.fill_file_list_local(directory=direc)
     
     fsync = FileSync(flists=flists_)
     fsync.compare_lists()
 
 if __name__ == '__main__':
-    build_indicies()
+    #build_indicies()
     compare_local()
