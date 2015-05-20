e7cd95849c3a1b7d782afc3a38bc5c32ef5fdf2a
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-20 16:09:20 -0400

    first sucessful run through

--- sync_app/sync_utils.py
+++ sync_app/sync_utils.py
@@ -15,6 +15,8 @@ from sync_app.util import run_command, cleanup_path
 from sync_app.file_cache import FileListCache
 from sync_app.file_sync import FileSync
 
+from apiclient.errors import UnknownFileType
+
 def get_md5_old(fname):
     """ python only md5 function """
     m = hashlib.md5()
@@ -33,28 +35,24 @@ def get_md5(fname):
 
 def build_gdrive_index():
     """ build GDrive index """
+    from sync_app.gdrive_instance import GdriveInstance
     from sync_app.file_list_gdrive import FileListGdrive
 
-    fcache = FileListCache(pickle_file='%s/.gdrive_file_list_cache.pkl.gz' %
-                           os.getenv('HOME'))
-    flist = FileListGdrive()
+    gdrive = GdriveInstance()
+    flist = FileListGdrive(gdrive=gdrive)
     #### always rebuild index
     ### (gdriveid isn't apparently persistent, also nothing saved by caching)
-    print('update cache')
+    print('download file metadata')
     flist.fill_file_list_gdrive()
-    print('save cache')
-    fcache.write_cache_file_list(flist.filelist)
+    return flist
 
 def build_s3_index():
     """ build S3 index """
     from sync_app.file_list_s3 import FileListS3
 
-    fcache = FileListCache(pickle_file='%s/.s3_file_list_cache.pkl.gz' %
-                           os.getenv('HOME'))
     flist = FileListS3()
-    flist = fcache.get_cache_file_list(file_list_class=FileListS3)
     flist.fill_file_list_s3()
-    fcache.write_cache_file_list(flist.filelist)
+    return flist
 
 def build_local_index(directories=None, rebuild_index=False):
     """ build local index """
@@ -64,31 +62,53 @@ def build_local_index(directories=None, rebuild_index=False):
         return False
     fcache = FileListCache(pickle_file='%s/.local_file_list_cache.pkl.gz' %
                            os.getenv('HOME'))
-    flist = FileListLocal()
+    flist_cache = FileListLocal()
     if not rebuild_index:
-        flist = fcache.get_cache_file_list(file_list_class=FileListLocal)
+        flist_cache = fcache.get_cache_file_list(file_list_class=FileListLocal)
+    print(len(flist_cache.filelist))
+    flist = FileListLocal(cache_file_list=flist_cache)
     for direc in directories:
         flist.fill_file_list_local(directory=direc)
+    print(len(flist.filelist))
     fcache.write_cache_file_list(flist.filelist)
+    return flist
 
 def build_indicies():
     """ build local/gdrive/s3 index """
     print('build gdrive')
-    build_gdrive_index()
-    print('build s3')
-    build_s3_index()
-    local_dirs = []
-    for basedir in ['/home/ddboline', '/media/sabrent2000',
-                    '/media/caviar2000', '/media/western2000']:
-        for subdir in ['Documents/AudioBooks', 'Documents/mp3',
-                       'Documents/podcasts', 'Documents/video', 'D0_Backup']:
-            local_dirs.append('%s/%s' % (basedir, subdir))
-    for basedir in ['/media/sabrent2000', '/media/caviar2000',
-                    '/media/western2000']:
-        for subdir in ['dilepton2_backup', 'dilepton_tower_backup']:
-            local_dirs.append('%s/%s' % (basedir, subdir))
-    print('build local %s' % ' '.join(local_dirs))
-    build_local_index(directories=local_dirs)
+    flist_gdrive = build_gdrive_index()
+    print('build local')
+    flist_local = build_local_index(directories=['/home/ddboline/gDrive'])
+    fsync = FileSync(flists=[flist_gdrive, flist_local])
+
+    def upload_file(finfo):
+        print('upload', finfo.filename)
+#        try:
+#            flist_gdrive.upload_file(finfo.filename)
+#        except UnknownFileType:
+#            return
+    
+    def download_file(finfo):
+        if 'https' in finfo.urlname:
+            print('download', finfo.urlname, finfo.exportpath)
+#            finfo.download()
+    
+    fsync.compare_lists(callback0=download_file, callback1=upload_file)
+    
+#    print('build s3')
+#    flist_s3 = build_s3_index()
+#    local_dirs = []
+#    for basedir in ['/home/ddboline', '/media/sabrent2000',
+#                    '/media/caviar2000', '/media/western2000']:
+#        for subdir in ['Documents/AudioBooks', 'Documents/mp3',
+#                       'Documents/podcasts', 'Documents/video', 'D0_Backup']:
+#            local_dirs.append('%s/%s' % (basedir, subdir))
+#    for basedir in ['/media/sabrent2000', '/media/caviar2000',
+#                    '/media/western2000']:
+#        for subdir in ['dilepton2_backup', 'dilepton_tower_backup']:
+#            local_dirs.append('%s/%s' % (basedir, subdir))
+#    print('build local %s' % ' '.join(local_dirs))
+#    build_local_index(directories=local_dirs)
 
 def compare_local(rebuild_index=True, directories=None):
     """ compare local directories """
