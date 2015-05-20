63a3d10b2805917f3d402fda2f04da62f5f7bad8
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-20 08:16:53 -0400

    docstrings

--- sync_app/s3_instance.py
+++ sync_app/s3_instance.py
@@ -1,5 +1,8 @@
-#! /usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+"""
+    S3 Instance class, manage connection to s3
+"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -8,8 +11,8 @@ from __future__ import unicode_literals
 import os
 import boto
 
-# read aws credentials from file, then stick into global variables...
 def read_keys():
+    """ read aws credentials from file, then stick into global variables... """
     with open('%s/.aws/credentials' % os.getenv('HOME'), 'r') as f:
         for line in f:
             if 'aws_access_key_id' in line:
@@ -21,17 +24,20 @@ def read_keys():
 AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY = read_keys()
 
 class S3Instance(object):
+    """ S3Instance class to interact with S3 via Boto """
     def __init__(self):
         self.s3 = boto.connect_s3(aws_access_key_id=AWS_ACCESS_KEY_ID,
                                   aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
     def get_list_of_buckets(self):
+        """ get list of buckets """
         _temp = []
         for bucket in self.s3.get_all_buckets():
             _temp.append(bucket.name)
         return _temp
 
     def get_list_of_keys(self, bucket_name=None, callback_fn=None):
+        """ get list of keys """
         if not callback_fn:
             def _temp_fn(x):
                 print(x.key)
