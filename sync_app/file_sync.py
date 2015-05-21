c7a143f4777180940dc447f449accd11abde8426
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-20 20:47:18 -0400

    ...

--- sync_app/file_sync.py
+++ sync_app/file_sync.py
@@ -30,21 +30,39 @@ class FileSync(object):
         if len(self.flists) < 2:
             return None
 
-        for flist in self.flists:
-            print(len(flist.filelist_name_dict))
-
         list_a_not_b = []
         list_b_not_a = []
 
-        for fn in self.flists[0].filelist_name_dict:
+        for fn, finfo0 in self.flists[0].filelist_name_dict.items():
             for flist in self.flists[1:]:
                 if fn not in flist.filelist_name_dict:
-                    list_a_not_b.append(self.flists[0].filelist_name_dict[fn])
+                    list_a_not_b.append(finfo0)
+#                else:
+#                    finfo1 = flist.filelist_name_dict[fn]
+#                    mt0 = int(finfo0[0].filestat.st_mtime)
+#                    md0 = finfo0[0].md5sum
+#                    sz0 = int(finfo0[0].filestat.st_size)
+#                    mt1 = int(finfo1[0].filestat.st_mtime)
+#                    md1 = finfo1[0].md5sum
+#                    sz1 = int(finfo1[0].filestat.st_size)
+#                    if mt0 > mt1 and md0 != md1:
+#                        print(mt0, mt1, md0, md1, sz0, sz1)
+#                        list_a_not_b.append(finfo0)
 
         for flist in self.flists[1:]:
-            for fn in flist.filelist_name_dict:
+            for fn, finfo1 in flist.filelist_name_dict.items():
                 if fn not in self.flists[0].filelist_name_dict:
-                    list_b_not_a.append(flist.filelist_name_dict[fn])
+                    list_b_not_a.append(finfo1)
+#                else:
+#                    finfo0 = self.flists[0].filelist_name_dict[fn]
+#                    mt0 = int(finfo0[0].filestat.st_mtime)
+#                    md0 = finfo0[0].md5sum
+#                    sz0 = int(finfo0[0].filestat.st_size)
+#                    mt1 = int(finfo1[0].filestat.st_mtime)
+#                    md1 = finfo1[0].md5sum
+#                    sz1 = int(finfo1[0].filestat.st_size)
+#                    if mt1 > mt0 and md0 != md1:
+#                        list_a_not_b.append(finfo1)
 
         for finf_ in list_a_not_b:
             if callback0:
