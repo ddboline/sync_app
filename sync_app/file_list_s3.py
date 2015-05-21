b37671d26fa8f2eec30f59e3864f68d2f6a9c540
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-21 18:16:46 -0400

    fill_item/__getitem__

--- sync_app/file_list_s3.py
+++ sync_app/file_list_s3.py
@@ -11,14 +11,27 @@ from __future__ import unicode_literals
 from sync_app.file_list import FileInfo, FileList
 from sync_app.s3_instance import S3Instance
 
+from dateutil.parser import parse
+
 class FileInfoS3(FileInfo):
     """ File Info for S3, add bucket metadata """
     __slots__ = FileInfo.__slots__ + ['bucket']
 
-    def __init__(self, fn='', md5='', fs=None, bk=''):
+    def __init__(self, fn='', md5='', fs=None, bk='', item=None):
         FileInfo.__init__(self, fn=fn, md5=md5, fs=fs)
         self.bucket = bk
-
+        if item:
+            self.fill_item(item)
+        
+    def fill_item(self, item):
+        self.filename = item.key
+        self.bucket = item.bucket.name
+        self.urlname = 's3://%s/%s' % (self.bucket, self.filename)
+        self.md5sum = item.etag.replace('"','')
+        _temp = {'st_size': item.size,
+                 'st_mtime': int(parse(item.last_modified).strftime("%s"))}
+        self.fill_stat(**_temp)
+        
 
 class FileListS3(FileList):
     """ File list for S3 """
@@ -27,22 +40,24 @@ class FileListS3(FileList):
         """ Init function """
         FileList.__init__(self, filelist=filelist, basedir=bucket,
                           filelist_type='s3')
-        self.bucketlist = []
         self.s3 = s3
-        pass
+        self.filelist_key_dict = {}
+
+    def __getitem__(self, key):
+        if key in self.filelist_key_dict:
+            return self.filelist_key_dict[key]
+        else:
+            return self.FileList.__getitem__(self, key)
 
     def append_item(self, item):
         """ append S3 item to filelist """
-        finfo = FileInfoS3()
-        finfo.filename = item.key
-        finfo.bucket = item.bucket.name
-        finfo.urlname = 's3://%s/%s' % (finfo.bucket, finfo.filename)
-        finfo.md5sum = item.etag.replace('"','')
-        if finfo.filename in self.filelist_name_dict:
+        finfo = FileInfoS3(item=item)
+        if finfo.filename in self:
             for ffn in self.filelist_name_dict[finfo.filename]:
                 if ffn.md5sum == finfo.md5sum:
                     return self.filelist_name_dict[finfo.filename]
         self.append(finfo)
+        self.filelist_key_dict[finfo.filename] = finfo
         return finfo
 
     def fill_file_list_s3(self, bucket=None):
