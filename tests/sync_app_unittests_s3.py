0f3433394a5247abd15d2d5d25282c29608825b4
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-21 18:18:19 -0400

    ...

--- tests/sync_app_unittests_s3.py
+++ tests/sync_app_unittests_s3.py
@@ -9,17 +9,13 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import os
-import hashlib
+#import hashlib
 import unittest
 
 CURDIR = os.path.abspath(os.curdir)
 os.sys.path.append(CURDIR)
 
 from sync_app.sync_utils import get_md5
-from sync_app.file_cache import FileListCache
-from sync_app.file_list_local import FileInfoLocal, FileListLocal
-from sync_app.file_list_gdrive import FileListGdrive
-from sync_app.gdrive_instance import GdriveInstance
 from sync_app.file_list_s3 import FileListS3
 from sync_app.s3_instance import S3Instance
 
@@ -27,138 +23,34 @@ TEST_FILE = 'tests/test_dir/hello_world.txt'
 TEST_DIR = 'tests/test_dir'
 TEST_GDR = 'Amazon-Gift-Card.pdf'
 
-class TestSyncApp(unittest.TestCase):
+class TestSyncAppS3(unittest.TestCase):
     """ SyncApp Unit Tests """
 
-    def tearDown(self):
-        """ remove temporary pickle file """
-        if os.path.exists('.tmp_file_list_cache.pkl.gz'):
-            os.remove('.tmp_file_list_cache.pkl.gz')
-        pass
-
-    def test_file_info_local(self):
-        """ Test FileInfoLocal class """
-        finfo = FileInfoLocal(fn=TEST_FILE)
-        output = ('%s' % finfo).replace(CURDIR, '')
-#        print('finfo', output)
-        m = hashlib.md5()
-        m.update(output)
-        self.assertEqual(m.hexdigest(), 'bb0681fe01c42429d0a7b0994f0d9265')
-
-    def test_file_list_local(self):
-        """ Test FileListLocal class """
-        flist = FileListLocal()
-        flist.fill_file_list_local(directory=TEST_DIR)
-        output = []
-        for fl in flist.filelist:
-            output.append(('%s' % fl).replace(CURDIR, ''))
-        output = sorted(output)
-#        print('file_list', '\n'.join(output))
-        m = hashlib.md5()
-        for out in sorted(output):
-            m.update(out)
-        self.assertEqual(m.hexdigest(), '30eceb5a3d7b9c761651f8b1df5e53b5')
-
-    def test_file_list_cache(self):
-        """ Test FileListCache class """
-        flist = FileListLocal()
-        flist.fill_file_list_local(directory=TEST_DIR)
-        fcache = FileListCache(pickle_file='.tmp_file_list_cache.pkl.gz')
-        fcache.write_cache_file_list(flist.filelist)
-        del flist
-        flist = fcache.get_cache_file_list()
-        output = []
-        for fl in flist.filelist:
-            output.append(('%s' % fl).replace(CURDIR, ''))
-        output = sorted(output)
-#        print('file_cache', '\n'.join(output))
-        m = hashlib.md5()
-        for out in sorted(output):
-            m.update(out)
-        self.assertEqual(m.hexdigest(), '30eceb5a3d7b9c761651f8b1df5e53b5')
-
-    def test_gdrive_list_files(self):
-        """ Test GdriveInstance.list_files """
-        self.test_title = None
-        def get_title(item):
-            self.test_title = item['title']
-        gdrive = GdriveInstance(number_to_process=10)
-        gdrive.list_files(get_title, searchstr=TEST_GDR)
-        m = hashlib.md5()
-        m.update(self.test_title)
-        self.assertEqual(m.hexdigest(), 'ee3ff087897ce88747e7c2b2fc0a59df')
-
-    def test_gdrive_upload_search_download_delete(self):
-        """ Test GdriveInstance.upload """
-        gdrive = GdriveInstance()
-        flist_gdrive = FileListGdrive(gdrive=gdrive)
-
-        gdrive.get_folders(flist_gdrive.append_dir)
-
-        flist_gdrive.create_directory(os.path.dirname(TEST_FILE))
-        fid = flist_gdrive.upload_file(TEST_FILE)
-        sstr = os.path.basename(TEST_FILE)
-        gdrive.list_files(flist_gdrive.append_item, searchstr=sstr)
-        gdrive.get_folders(flist_gdrive.append_dir)
-
-        flist_gdrive.filelist_id_dict[fid].download()
-        fname = '/home/ddboline/gDrive/tests/test_dir/hello_world.txt'
-
-        gdrive.delete_file(fid)
-        flist_gdrive.delete_directory(os.path.dirname(TEST_FILE))
-        self.assertEqual(flist_gdrive.filelist_id_dict[fid].exportpath,
-                         '/home/ddboline/gDrive/tests/test_dir')
-        self.assertEqual(flist_gdrive.filelist_id_dict[fid].md5sum,
-                         '8ddd8be4b179a529afa5f2ffae4b9858')
-        self.assertEqual(get_md5(fname), '8ddd8be4b179a529afa5f2ffae4b9858')
-        os.remove(fname)
-
-    def test_gdrive_search_directory(self):
-        """ Test FileListGdrive """
-        gdrive = GdriveInstance()
-        flist_gdrive = FileListGdrive()
-        gdrive.get_folders(flist_gdrive.append_dir)
-        flist_gdrive.fix_export_path()
-        id_ = flist_gdrive.directory_name_dict['share'].gdriveid
-        val = flist_gdrive.directory_id_dict[id_]
-        self.assertEqual(val.exportpath,
-                         '/home/ddboline/gDrive/ATLAS/code/' +
-                         'ISF_Calo_Validation/17.2.4.10')
-
-    def test_gdrive_list_directories(self):
-        """ Test FileListGdrive """
-        gdrive = GdriveInstance()
-        flist_gdrive = FileListGdrive(gdrive=gdrive)
-        gdrive.get_folders(flist_gdrive.append_dir)
-        flist_gdrive.fix_export_path()
-
-        finf_ = flist_gdrive.directory_name_dict['share']
-
-        self.assertEqual(finf_.exportpath,
-                         '/home/ddboline/gDrive/ATLAS/code/' +
-                         'ISF_Calo_Validation/17.2.4.10')
-
-    def test_gdrive_create_directory(self):
-        """ Test GdriveInstance.insert """
-        gdrive = GdriveInstance()
-        body_obj = {'title': 'test_directory',
-                    'mimeType': 'application/vnd.google-apps.folder'}
-        request = gdrive.service.files().insert(body=body_obj)
-        response = request.execute()
-        flist_gdrive = FileListGdrive()
-        gdrive.get_folders(flist_gdrive.append_dir)
-        flist_gdrive.fix_export_path()
-        fid = response['id']
-        gdrive.delete_file(fid)
-        self.assertEqual('test_directory',
-                         flist_gdrive.filelist_id_dict[fid].filename)
+    def setUp(self):
+        self.s3 = S3Instance()
+        self.flist_s3 = FileListS3(s3=self.s3)
 
     def test_s3_list_files(self):
-        s3 = S3Instance()
-        s3_list = FileListS3(s3=s3)
+        for bucket in self.s3.get_list_of_buckets():
+            self.s3.get_list_of_keys(bucket_name=bucket,
+                                     callback_fn=self.flist_s3.append_item)
+        md_ = self.flist_s3['2015-01-01.txt'].md5sum
+        self.assertEqual(md_, '866c3c2d566d44b88e1e4a4fc1e7d65d')
         
-        print(s3.get_list_of_buckets())
-        
+    def test_s3_upload_search_download_delete(self):
+        bname = 'test_bucket_ddbline_20150521'
+        self.s3.create_bucket(bname)
+        self.s3.upload(bname, TEST_FILE)
+        self.s3.get_list_of_keys(bucket_name=bname,
+                                 callback_fn=self.flist_s3.append_item)
+        md5_ = self.flist_s3[TEST_FILE].md5sum
+        self.assertEqual(md5_, '8ddd8be4b179a529afa5f2ffae4b9858')
+        self.s3.download(bname, TEST_FILE, 'tests/test_dir/test.txt')
+        self.s3.delete_key(bname, TEST_FILE)
+        self.s3.delete_bucket('test_bucket_ddbline_20150521')
+        md5_ = get_md5('tests/test_dir/test.txt')
+        self.assertEqual(md5_, '8ddd8be4b179a529afa5f2ffae4b9858')
+        os.remove('tests/test_dir/test.txt')
 
 if __name__ == '__main__':
     unittest.main()
