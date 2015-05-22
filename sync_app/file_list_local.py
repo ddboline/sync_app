e895b390326d74059b8737a75a26fd1433b183ce
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-22 17:16:31 -0400

    in_tuple, change FileList.filelist to dict

--- sync_app/file_list_local.py
+++ sync_app/file_list_local.py
@@ -17,14 +17,15 @@ from sync_app.file_list import FileInfo, FileList
 class FileInfoLocal(FileInfo):
     """ File Info Local """
 
-    def __init__(self, fn='', md5='', fs=None):
+    def __init__(self, fn='', md5='', fs=None, in_tuple=None):
         """ Init function """
         absfn = os.path.abspath(fn)
         if not os.path.exists(absfn):
             print('ERROR')
             return False
         _url = 'file://%s' % absfn
-        FileInfo.__init__(self, fn=absfn, url=_url, md5=md5, fs=fs)
+        FileInfo.__init__(self, fn=absfn, url=_url, md5=md5, fs=fs,
+                          in_tuple=in_tuple)
 
     def get_md5(self):
         """ Wrapper around sync_utils.get_md5 """
@@ -57,6 +58,7 @@ class FileListLocal(FileList):
         def parse_dir(arg, path, filelist):
             """ Parse directory, fill FileInfo object """
             for fn in filelist:
+#                print(fn)
                 finfo = None
                 fullfn = os.path.abspath('%s/%s' % (path, fn))
                 fullfn = fullfn.replace('//', '/')
@@ -66,23 +68,23 @@ class FileListLocal(FileList):
                     if self.cache_file_list and \
                             fn in self.cache_file_list.filelist_name_dict:
                         flist_ = self.cache_file_list.filelist_name_dict
-                        finf_ = flist_[fn][0]
-                        if fn not in self.filelist_name_dict:
-                            self.append(finf_)
+                        for finf_ in flist_[fn]:
+                            if finf_.filename == fullfn:
+                                self.append(finf_)
 
                     if fn in self.filelist_name_dict:
                         for ffn in self.filelist_name_dict[fn]:
+                            if finfo:
+                                continue
                             if fullfn == ffn.filename:
-                                if fs.st_mtime > ffn.filestat.st_mtime:
+                                mt0 = int(fs.st_mtime)
+                                mt1 = int(ffn.filestat.st_mtime)
+                                if mt0 > mt1:
                                     finfo = FileInfoLocal(fn=fullfn)
-                                    if self.do_debug:
-                                        print(finfo)
                                 else:
                                     finfo = ffn
                     if not finfo:
                         finfo = FileInfoLocal(fn=fullfn)
-                        if self.do_debug:
-                            print(finfo)
                         self.append(finfo)
 
         if type(directory) in (str, unicode):
