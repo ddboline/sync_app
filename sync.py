fc2661030db25778104ccb72b6ee397d4fc24249
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-22 01:16:58 -0400

    ...

--- sync.py
+++ sync.py
@@ -14,7 +14,9 @@ from __future__ import unicode_literals
 
 #import os
 
-from sync_app.sync_utils import sync_gdrive
+from sync_app.sync_utils import sync_gdrive, sync_s3, sync_local
 
 if __name__ == '__main__':
-    sync_gdrive(dry_run=True)
+#    sync_gdrive(dry_run=True)
+#    sync_s3(dry_run=True)
+    sync_local(dry_run=True)
