b793e45eafc94fe01e0f831465d2063c7972bbf8
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-22 17:14:26 -0400

    add_filelist_to_cache

--- sync_app/file_cache.py
+++ sync_app/file_cache.py
@@ -54,22 +54,31 @@ class FileListCache(object):
         return True
 
     def get_cache_file_list(self, file_list_obj=None,
+                            file_info_class=FileInfo,
                             file_list_class=FileList):
         """ read list from cache file """
         if not file_list_obj:
             file_list_obj = file_list_class()
-        _temp = self.read_pickle_object_in_file()
-        if _temp:
-            self.cache_file_list_dict = _temp
-        for finfo in self.cache_file_list_dict.values():
-            file_list_obj.append(finfo)
+        temp_ = self.read_pickle_object_in_file()
+        if temp_:
+            for tup_ in temp_:
+                finf_ = file_info_class(in_tuple=tup_)
+                fn_ = finf_.filename
+                self.cache_file_list_dict[fn_] = finf_
+                file_list_obj.append(finf_)
         return file_list_obj
 
-    def write_cache_file_list(self, file_list=None):
+    def add_filelist_to_cache(self, file_list=None):
         if not file_list:
             return False
         for fileinfo in file_list:
             fn = fileinfo.filename
-            if fn not in self.cache_file_list_dict:
-                self.cache_file_list_dict[fn] = fileinfo
-        return self.write_pickle_object_to_file(self.cache_file_list_dict)
+            self.cache_file_list_dict[fn] = fileinfo
+
+    def write_cache_file_list(self, file_list=None):
+        if file_list:
+            self.add_filelist_to_cache(file_list)
+        cache_list = []
+        for finfo in self.cache_file_list_dict.values():
+            cache_list.append(finfo.output_cache_tuple())
+        return self.write_pickle_object_to_file(tuple(cache_list))
