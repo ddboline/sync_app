9f04d20f00b4219e95f97b56597ce2dcff5b6949
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-23 15:34:15 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -3,5 +3,5 @@
 sudo apt-get update
 sudo apt-get install -y python-boto python-googleapi python-dateutil
 
-# scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/aws_scripts/keys_20150119.tar.gz .
+scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/client_secrets.json .
 scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/*.dat .
