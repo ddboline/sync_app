b1c09f17f19d38f8380f55d7fc008e936431238a
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-06 18:38:23 -0400

    ...

---
+++ setup_conda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo /opt/conda/bin/conda install --yes boto dateutil pip pytz 
+
+sudo /opt/conda/bin/pip install --upgrade google-api-python-client
+
+scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/client_secrets.json sync_app/
+scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/*.dat .
