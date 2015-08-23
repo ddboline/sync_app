2ae651c300fc02163098bfe7c72c8f051bd2ae1a
Daniel Boline <ddboline@gmail.com>
2015-08-23 19:50:17 -0400

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-./tests/sync_app_unittests_local.py
-./tests/sync_app_unittests_s3.py
-./tests/sync_app_unittests_gdrive.py
+nosetests tests/*.py sync_app/*.py
--- test_conda.sh
+++ test_conda.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-python3 ./tests/sync_app_unittests_local.py
-python3 ./tests/sync_app_unittests_s3.py
-python3 ./tests/sync_app_unittests_gdrive.py
+nosetests tests/*.py sync_app/*.py
