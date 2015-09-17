1460e485da972b979eb621b78fdf6879863f53e4
Daniel Boline <ddboline@gmail.com>
2015-09-17 15:54:02 -0400

    list_drive_file

---
+++ list_drive_files.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+    list gdrive files
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+#import os
+
+from sync_app.sync_utils import list_drive_parse
+
+if __name__ == '__main__':
+    list_drive_parse()
