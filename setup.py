5a01e3a6f4055dd1251d06a3dba5406241c7dc3a
Daniel Boline <ddboline@gmail.com>
2015-09-23 15:10:37 -0400

    ...

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Created on Fri May 22 18:29:26 2015
 
 @author: ddboline
 """
-from __future__ import print_function
+from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 #from __future__ import unicode_literals
