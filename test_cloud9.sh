4ba834cfb33e4c0f3c67cee5edfb557f61910446
Daniel Boline <ddboline@gmail.com>
2015-09-30 18:59:52 -0400

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 nosetests --with-coverage --cover-package=sync_app tests/*.py sync_app/*.py
+
+# pyreverse sync_app
+# for N in classes packages; do dot -Tps ${N}*.dot > ${N}.ps ; done
