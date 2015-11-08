d8073f183cb26ed78e0efb50a4e16c09787c5842
Daniel Boline <ddboline@gmail.com>
2015-11-08 13:27:41 -0500

    remove exporturls, etc

--- sync_app/file_info_onedrive.py
+++ sync_app/file_info_onedrive.py
@@ -14,8 +14,8 @@ from .file_info import FileInfo
 
 BASE_DIR = '%s/OneDrive' % os.getenv('HOME')
 
-FILE_INFO_SLOTS = ('gdriveid', 'mimetype', 'parentid', 'exporturls',
-                   'exportpath', 'isroot', 'onedrive')
+FILE_INFO_SLOTS = ('onedriveid', 'mimetype', 'exportpath',
+                   'onedrive', 'sha1sum', 'parentid')
 
 
 class FileInfoOneDrive(FileInfo):
@@ -23,15 +23,13 @@ class FileInfoOneDrive(FileInfo):
     __slots__ = FileInfo.__slots__ + list(FILE_INFO_SLOTS)
 
     def __init__(self, gid='', fn='', md5='', onedrive=None, item=None,
-                 in_tuple=None, mime='', pid=None):
+                 in_tuple=None, mime='', sha1sum=None):
         FileInfo.__init__(self, fn=fn, md5=md5)
         self.onedriveid = gid
-        self.isroot = False
-        self.exporturls = {}
         self.onedrive = onedrive
         self.mimetype = mime
-        self.parentid = pid
         self.exportpath = ''
+        self.sha1sum = ''
         if item:
             self.fill_item(item)
         if in_tuple:
@@ -44,12 +42,12 @@ class FileInfoOneDrive(FileInfo):
     def download(self):
         """ wrapper around OneDriveInstance.download """
         if BASE_DIR in self.filename:
-            return self.onedrive.download(self.urlname, self.filename,
-                                          md5sum=self.md5sum)
+            return self.onedrive.download(self.onedriveid, self.filename,
+                                          sha1sum=self.sha1sum)
         else:
             path_ = '%s/%s' % (BASE_DIR, self.filename)
             return self.onedrive.download(self.urlname, path_,
-                                          md5sum=self.md5sum)
+                                          sha1sum=self.sha1sum)
 
     def __repr__(self):
         """ nice string representation """
@@ -57,27 +55,31 @@ class FileInfoOneDrive(FileInfo):
                'url=%s, ' % self.urlname +\
                'path=%s, ' % self.exportpath +\
                'md5=%s, ' % self.md5sum +\
+               'sha1sum=%s, ' % self.sha1sum +\
                'size=%s, ' % self.filestat.st_size +\
                'st_mime=%s, ' % self.filestat.st_mtime +\
-               'id=%s, ' % self.onedriveid +\
-               'pid=%s, ' % self.parentid +\
-               'isroot=%s)>' % self.isroot
+               'id=%s)>' % self.onedriveid
 
     def output_cache_tuple(self):
-        return (self.filename, self.urlname, self.md5sum,
+        return (self.filename, self.urlname, self.md5sum, self.sha1sum,
                 self.filestat.st_mtime, self.filestat.st_size, self.onedriveid,
-                self.mimetype, self.parentid, self.exporturls, self.exportpath,
-                self.isroot, self.onedrive)
+                self.mimetype, self.exportpath, self.onedrive)
 
     def input_cache_tuple(self, in_tuple):
-        self.filename, self.urlname, self.md5sum, self.filestat.st_mtime, \
-            self.filestat.st_size, self.onedriveid, self.mimetype, \
-            self.parentid, self.exporturls, self.exportpath, self.isroot, \
-            self.onedrive = in_tuple
+        self.filename, self.urlname, self.md5sum, self.sha1sum, \
+            self.filestat.st_mtime, self.filestat.st_size, self.onedriveid, \
+            self.mimetype, self.exportpath, self.onedrive = in_tuple
 
     def fill_item(self, item):
         """ fill FileInfoOneDrive from item """
-        raise NotImplementedError
+        self.onedriveid = item['id']
+        self.filename = item['name']
+        self.parentid = item['parentid']
+        if 'file' in item:
+            self.mimetype = item['file'].get('mimeType', 'None')
+            if 'hashes' in item['file']:
+                self.sha1sum = item['file']['hashes'].get('sha1Hash',
+                                                          '').lower()
 
 
 def test_file_info_onedrive():
