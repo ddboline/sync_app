5485143db3f5cf6c1111363f78f1f48f84380db4
Daniel Boline <ddboline@gmail.com>
2015-11-09 15:02:46 -0500

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+sudo cp -a ${HOME}/.ssh /root/
+sudo chown -R root:root /root/
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
-sudo apt-get install -y python-boto python-googleapi python-dateutil python-nose python-coverage
+sudo apt-get install -y python-boto python-googleapi python-dateutil python-nose python-coverage python-onedrivesdk
 
 scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/client_secrets.json sync_app/
 scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/*.dat .
