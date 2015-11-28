97623886ed6eabfaf716625cbed887fd571f4da7
Daniel Boline <ddboline@gmail.com>
2015-11-28 14:00:14 -0500

    ...

--- sync_app/file_list_local.py
+++ sync_app/file_list_local.py
@@ -3,10 +3,8 @@
 """
     extract FileInfo object for local files
 """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 
 import os
 
