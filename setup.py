f7846a4cd38932cf29c0c538efe1dc9ed5ea3e54
Daniel Boline <ddboline@gmail.com>
2016-11-05 14:06:17 -0400

    ...

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name='sync_app',
-    version='0.0.3.3',
+    version='0.0.3.4',
     author='Daniel Boline',
     author_email='ddboline@gmail.com',
     description='sync_app',
