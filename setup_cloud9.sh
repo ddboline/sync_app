78360615099b6db5641f89aba951de50ba8cbebb
Daniel Boline <ddboline@gmail.com>
2016-12-04 18:51:57 -0500

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -2,7 +2,7 @@
 
 sudo cp -a ${HOME}/.ssh /root/
 sudo chown -R root:root /root/
-sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/xenial/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes python-boto python-googleapi python-dateutil python-nose python-coverage \
                                     python-onedrivesdk python-setuptools python-dev python-futures
