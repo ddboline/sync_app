ffb04a193ac56ba09181925c0b7be3d621dc7e43
Daniel Boline <ddboline@gmail.com>
2016-12-21 07:00:50 -0500

    version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name='sync_app',
-    version='0.0.3.4',
+    version='0.0.3.5',
     author='Daniel Boline',
     author_email='ddboline@gmail.com',
     description='sync_app',
